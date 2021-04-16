adceb7383c4f99351f92c8abe6153b9ec6978741
allya-tt <73088897+allya-tt@users.noreply.github.com>
2021-04-16 20:42:31 +0700

    Create search.py

---
+++ search.py
@@ -0,0 +1,30 @@
+def poisk(a, n):
+    first=0
+    last=len(a)-1
+    mid=len(a)//2
+    while first<=last:
+        if element>a[mid]:
+            first=mid+1
+        elif element<a[mid]:
+            last=mid-1
+        elif element==a[mid]:
+            return mid
+        mid=(last+first)//2
+    if first > last:
+        return None
+def recursion_poisk(a, first, last):
+    mid = (first+last) // 2
+    while first!=last and first<=last:
+        if element>a[mid]:
+            return recursion_poisk(a, mid+1, last)
+        elif element==a[mid]:
+            return mid
+        return recursion_poisk(a, first, mid-1)
+    return None
+a = list(map(int, input().split()))
+n=len(a)
+element=int(input())
+print(poisk(a,n))
+first=0
+last=len(a)-1
+print(recursion_poisk(a, first, last))
